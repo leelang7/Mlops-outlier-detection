de1491d43020e7e9bfb61ba91e51f982bc1918b7
Fixer <67309088+leelang7@users.noreply.github.com>
2024-04-26 20:47:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import matplotlib
 matplotlib.use('Agg')  # 백엔드를 Agg로 설정
-
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from alibi_detect.utils.saving import load_detector
 from alibi_detect.od import OutlierVAE
 from alibi_detect.utils.visualize import plot_instance_score, plot_feature_outlier_image
@@ -71,6 +70,25 @@ def home():
             return render_template('index.html', prediction="Anomaly Score: {:.4f}".format(od_preds['data']['instance_score'][0]), instance_score_plot=instance_score_plot_path, feature_outlier_image=feature_outlier_image_path)
 
     return render_template('index.html', prediction=None)
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+                img = Image.open(file.stream).convert('RGB')
+                img = img.resize((64, 64))
+                img = np.asarray(img)
+                img = img.astype(np.float32) / 255.
+                img = img.reshape(1, 64, 64, 3)
+
+                # Reconstruction
+                x_recon = od.vae(img).numpy()
+
+                # Prediction
+                od_preds = od.predict(img, outlier_type='instance', return_feature_score=True, return_instance_score=True)
+                instance_score = od_preds['data']['instance_score'][0]
+                return jsonify(prediction="Anomaly Score: {:.4f}".format(instance_score))
 
 if __name__ == '__main__':
     app.run(debug=True)
